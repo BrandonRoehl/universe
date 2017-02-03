112fb02c3237f3f9a8bacb07e4617b385e71d81e
Brandon Roehl <BrandonRoehl@gmail.com>
2017-02-02 19:38:49 -0600

    Built the universe

--- utils/Compile.rb
+++ utils/Compile.rb
@@ -1,21 +1,34 @@
 module Compile
+    @universe = $0.chomp('/utils/universe')
+
     def all(dir)
         self.symlink dir
         self.gcc dir
     end
 
     def symlink(dir)
-        Dir.glob('*').select do |file|
+        print 'Added '
+        Dir.glob("#{dir}/*").select do |file|
             file =~ /.[^.]*/ || file =~ /\.(rb|bash|sh|zsh)/
         end.each do |file|
-            puts file
+            comp = File.basename file
+            extn = File.extname  file
+            name = File.basename file, extn
+            FileUtils.symlink("../sources/#{File.basename dir}/#{comp}", "#{@universe}/bin/#{name}", force: true)
+            print " #{name}"
         end
+        puts
     end
 
     def gcc(dir)
-        for file in Dir.glob("*.{c,cpp,c++}")
-            puts file
+        print 'Compiled '
+        for file in Dir.glob("#{dir}/*.{c,cpp,c++}")
+            extn = File.extname  file
+            name = File.basename file, extn
+            system "gcc -std=gnu99 '#{file}' -o '#{@universe}/bin/#{name}'"
+            print " #{name}"
         end
+        puts
     end
 end
 include Compile
